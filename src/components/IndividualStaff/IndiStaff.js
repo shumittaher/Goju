323027b2e6adb478f6eebf3f7bd59e4a32ac464f
shumit <shumitt@gmail.com>
2021-10-05 13:05:28 +0600

    bugfix

--- src/components/IndividualStaff/IndiStaff.js
+++ src/components/IndividualStaff/IndiStaff.js
@@ -7,16 +7,18 @@ const IndiStaff = (props) => {
 
     return (
 
-        <div className="w-72 h-96 shadow-2xl group">
+        <div className="w-72 h-96 shadow-2xl group ">
 
-            <img className="w-72 " src={`https://template.hasthemes.com/judo/judo/img/trainer/${image}`} alt="" />
 
-            <div className="text-white relative bottom-32 bg-green-400 p-5 shadow-2xl group-hover:bg-green-700">
+            <div className="text-white relative top-64 bg-green-400 p-5 shadow-2xl z-10">
                 <p className="font-bold	text-2xl">{Name}</p>
                 <p>{Designation}</p>
             </div>
 
-            <div className="bg-white relative bottom-32 p-5 shadow-2xl opacity-0 group-hover:opacity-100">
+            <img className="w-72 relative bottom-32 z-0" src={`https://template.hasthemes.com/judo/judo/img/trainer/${image}`} alt="" />
+
+
+            <div className="bg-white relative bottom-full z-20 p-5 shadow-2xl opacity-0 group-hover:opacity-100">
                 <p>{descrip}</p>
             </div>
 
